cb2249f73cb1d8940e89c97acb7bf0c9d8891f18
ho1115 <102739523+ho1115@users.noreply.github.com>
2023-06-15 17:18:34 +0800

    Update rotHsh.c

--- rotHsh.c
+++ rotHsh.c
@@ -1,101 +1,207 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
-typedef struct Stringgs strs;
+typedef struct Hashnode hNod;
 
-struct Stringgs {
-  char* cont;
-  int val;
-  strs* prev;
-  strs* next;
+struct Hashnode {
+  int idx; int cnt; 
 };
 
 
 int orgHash(char* str, int len, int P) {
   int hash = 0;
-  for (int i = 0; i < len; i++) {hash = ((hash*27) % P + str[i] - 96) % P;}
-  return hash;     
+  for (int i = 0; i < len; i++) {hash = ((hash*26) % P + str[i] - 97) % P;}
+  return hash < 0 ? hash + P : hash;     
 }
 
-int sameCheck(char* str1, char* str2, int len, int shift) {
+int shiftHash(char* str, int len, int P, int shift) {
+  int hash = 0;
+  for (int i = 0; i < len; i++) {hash = ((hash*26) % P + str[(i+shift)%len] - 97) % P;}
+  return hash < 0 ? hash + P : hash;     
+}
+
+int sameCheck(char* str1, char* str2, int len, int shift, int cnt) {
+  if (cnt == 0) {return 0;}
   for (int i = 0; i < len; i++) {if (str1[(i+shift)%len] != str2[i]) {return 0;}}
   return 1;     
 }
 
 int main () {
-  int N, Q, ope, len, hash, shift, P = 1000000181;
+  int N, Q, ope, len, hash, tmphash, sechash, shift, cond = 0, P = 10000103, P2 = 10000453;
   long long pairs = 0;
   char* tmp = (char*) malloc(sizeof(char) * 1000001); 
   scanf("%d%d", &N, &Q);
-  strs* set = (strs*) malloc(sizeof(strs) * (N+Q));
-  strs* head = set;
-  strs* ptr = NULL;
-  strs* last = set[N-1];
-  char* strpool = (char*) malloc(sizeof(char) * (N+Q) * (len+1));
+  hNod* table = (hNod*) calloc(P, sizeof(hNod));
   scanf("%s", tmp);
   len = strlen(tmp);
+  char* strpool = (char*) malloc(sizeof(char) * (N+Q) * (len+1));
   char* str = (char*) malloc(sizeof(char) * (len+1));
   strcpy(str, tmp);
   free(tmp);
-  int h = (int) ceil(pow(27, len-1)) % P;
-  set[0].prev = NULL;
-  set[0].next = NULL;
+  int h = 26;
+  for (int i = 0; i < len-2; i++) {
+    h = (h * 26) % P;
+    if (h < 0) {h += P;}
+  }
   for (int i = 0; i < N; i++) {
-    if (i != 0) {
-      scanf("%s", str);
-      set[i].prev = &set[i-1];
-      set[i].next = NULL;
-    }
-    set[i].cont = strpool + i*(len+1);
-    strcpy(strpool + i*(len+1), str);
+    if (i != 0) {scanf("%s", str);}
     hash = orgHash(str, len, P);
-    if (hash < 0) {hash += P;}
-    set[i].val = hash;
+    sechash = orgHash(str, len, P2);
+    tmphash = hash;
+    if (table[hash].cnt == 0) {
+        table[hash].cnt++;
+        table[hash].idx = i;
+        strcpy(strpool + i*(len+1), str);
+    } else {
+        if (sameCheck(str, strpool + table[hash].idx*(len+1), len, 0,  table[hash].cnt) == 1) {
+            pairs += table[hash].cnt;
+            table[hash].cnt++;
+        } else {
+            tmphash = (tmphash + sechash) % P;
+            if (tmphash < 0) {tmphash += P;}
+            while (table[tmphash].cnt > 0 && sameCheck(str, strpool + table[tmphash].idx*(len+1), len, 0, table[tmphash].cnt) == 0) {
+                tmphash = (tmphash + sechash) % P;
+                if (tmphash < 0) {tmphash += P;}
+            } 
+            if (table[tmphash].cnt == 0) {
+                table[tmphash].cnt++;
+                table[tmphash].idx = i;
+                strcpy(strpool + i*(len+1), str);
+            } else {
+                pairs += table[tmphash].cnt;
+                table[tmphash].cnt++;
+            }
+        }
+    }
     for (int j = 0; j < len; j++) {
-      shift = j;
-      ptr = head;
-      if (j != 0) {
-        hash = (27*((hash-(set[i].cont[j-1] - 96)*h) % P) + set[i].cont[j-1] - 96) % P;
-        if (hash < 0) {hash += P;}
-      }
-      while (ptr && ptr != set[i]) {
-        if (ptr->val == hash && sameCheck(set[i].cont, ptr->cont, len, shift) == 1) {pairs++;}
-        ptr = ptr->next;
-      }
+        if (j == 0) {
+            tmphash = hash;
+            continue;
+        }
+        shift = j;        
+        tmphash = (26*((tmphash-(str[j-1] - 97)*h) % P) + str[j-1] - 97) % P; 
+        if (tmphash < 0) {tmphash += P;}
+        if (tmphash == hash) {break;}    
+        if (table[tmphash].cnt == 0) {continue;} 
+        else if (sameCheck(str, strpool + table[tmphash].idx*(len+1), len, shift,  table[tmphash].cnt) == 1) {pairs += table[tmphash].cnt;} 
+        else {
+            sechash = shiftHash(str, len, P2, shift);
+            tmphash = (tmphash + sechash) % P;
+            if (tmphash < 0) {tmphash += P;}
+            while (table[tmphash].cnt > 0 && sameCheck(str, strpool + table[tmphash].idx*(len+1), len, 0, table[tmphash].cnt) == 0) {
+                tmphash = (tmphash + sechash) % P;
+                if (tmphash < 0) {tmphash += P;}
+            } 
+            if (table[tmphash].cnt > 0) {pairs += table[tmphash].cnt;}
+        }
     }
   }
+  printf("%lld\n", pairs);
   for (int i = 0; i < Q; i++) {    
     scanf("%d", &ope);
     scanf("%s", str);
     if (ope == 1) {      
-      set[i+N].prev = last;
-      set[i+N].next = NULL;
-      set[i+N].cont = strpool + (i+N)*(len+1);
-      last = set[i+N];
-      strcpy(strpool + (i+N)*(len+1), str);
       hash = orgHash(str, len, P);
-      if (hash < 0) {hash += P;}
-      set[i+N].val = hash;
-      for (int j = 0; j < len; j++) {
-        shift = j;
-        ptr = head;
-        if (j != 0) {
-          hash = (27*((hash-(set[i+N].cont[j-1] - 96)*h) % P) + set[i+N].cont[j-1] - 96) % P;
-          if (hash < 0) {hash += P;}
+      sechash = orgHash(str, len, P2);
+      tmphash = hash;
+      if (table[hash].cnt == 0) {
+        table[hash].cnt++;
+        table[hash].idx = i+N;
+        strcpy(strpool + (i+N)*(len+1), str);
+      } else {
+          if (sameCheck(str, strpool + table[hash].idx*(len+1), len, 0,  table[hash].cnt) == 1) {
+            pairs += table[hash].cnt;
+            table[hash].cnt++;
+          } else {
+              tmphash = (tmphash + sechash) % P;
+              if (tmphash < 0) {tmphash += P;}
+              while (table[tmphash].cnt > 0 && sameCheck(str, strpool + table[tmphash].idx*(len+1), len, 0, table[tmphash].cnt) == 0) {
+                tmphash = (tmphash + sechash) % P;
+                if (tmphash < 0) {tmphash += P;}
+              } 
+              if (table[tmphash].cnt == 0) {
+                table[tmphash].cnt++;
+                table[tmphash].idx = i+N;
+                strcpy(strpool + (i+N)*(len+1), str);
+              } else {
+                pairs += table[tmphash].cnt;
+                table[tmphash].cnt++;
+              }
+            }
         }
-        while (ptr && ptr != set[i+N]) {
-          if (ptr->val == hash && sameCheck(set[i+N].cont, ptr->cont, len, shift) == 1) {pairs++;}
-          ptr = ptr->next;
+
+      for (int j = 0; j < len; j++) {
+        if (j == 0) {
+            tmphash = hash;
+            continue;
+        }
+        shift = j;        
+        tmphash = (26*((tmphash-(str[j-1] - 97)*h) % P) + str[j-1] - 97) % P;
+        if (tmphash < 0) {tmphash += P;}
+        if (tmphash == hash) {break;}    
+        if (table[tmphash].cnt == 0) {continue;} 
+        else if (sameCheck(str, strpool + table[tmphash].idx*(len+1), len, shift, table[tmphash].cnt) == 1) {pairs += table[tmphash].cnt;} 
+        else {
+            sechash = shiftHash(str, len, P2, shift);
+            tmphash = (tmphash + sechash) % P;
+            if (tmphash < 0) {tmphash += P;}
+            while (table[tmphash].cnt > 0 && sameCheck(str, strpool + table[tmphash].idx*(len+1), len, 0, table[tmphash].cnt) == 0) {
+                tmphash = (tmphash + sechash) % P;
+                if (tmphash < 0) {tmphash += P;}
+            } 
+            if (table[tmphash].cnt > 0) {pairs += table[tmphash].cnt;}
         }
       }
     } else {
-      ptr = head; 
-      while () { 
+        hash = orgHash(str, len, P);
+        sechash = orgHash(str, len, P2);
+        tmphash = hash;
+        if (table[hash].cnt == 0) {continue;} 
+        else {
+          if (sameCheck(str, strpool + table[hash].idx*(len+1), len, 0,  table[hash].cnt) == 1) {            
+            table[hash].cnt--;
+            pairs -= table[hash].cnt;
+          } else {
+              tmphash = (tmphash + sechash) % P;
+              if (tmphash < 0) {tmphash += P;}
+              while (table[tmphash].cnt > 0 && sameCheck(str, strpool + table[tmphash].idx*(len+1), len, 0, table[tmphash].cnt) == 0) {
+                tmphash = (tmphash + sechash) % P;
+                if (tmphash < 0) {tmphash += P;}
+              } 
+              if (table[tmphash].cnt == 0) {continue;}
+              else {
+                table[tmphash].cnt--;
+                pairs -= table[tmphash].cnt;
+              }
+            }
+        }
 
+      for (int j = 0; j < len; j++) {
+        if (j == 0) {
+            tmphash = hash;
+            continue;
+        }
+        shift = j;        
+        tmphash = (26*((tmphash-(str[j-1] - 97)*h) % P) + str[j-1] - 97) % P;
+        if (tmphash < 0) {tmphash += P;}
+        if (tmphash == hash) {break;}    
+        if (table[tmphash].cnt == 0) {continue;} 
+        else if (sameCheck(str, strpool + table[tmphash].idx*(len+1), len, shift, table[tmphash].cnt) == 1) {pairs -= table[tmphash].cnt;} 
+        else {
+            sechash = shiftHash(str, len, P2, shift);
+            tmphash = (tmphash + sechash) % P;
+            if (tmphash < 0) {tmphash += P;}
+            while (table[tmphash].cnt > 0 && sameCheck(str, strpool + table[tmphash].idx*(len+1), len, 0, table[tmphash].cnt) == 0) {
+                tmphash = (tmphash + sechash) % P;
+                if (tmphash < 0) {tmphash += P;}
+            } 
+            if (table[tmphash].cnt > 0) {pairs -= table[tmphash].cnt;}
+        }
       }
     }
+    printf("%lld\n", pairs);
   }
+  
   return 0;
 }
